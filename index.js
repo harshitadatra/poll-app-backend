7ca73a58a069bba7b1d16802454b635d913e4d9f
harshitadatra <harshitkiran22@gmail.com>
2025-02-16 13:47:06 +0530

    fix: token extraction

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const pollRoutes = require("./routes/pollRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// ✅ Use CORS Middleware
+app.use(cors({
+  origin: "http://localhost:5173", // Allow frontend origin
+  credentials: true, // Allow cookies & headers like Authorization
+}));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers["authorization"];
+    const authHeader= req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.status(401).json({ error: "Access denied" });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
