2d85e03456aa5091cc5f6c874674d6bb3ad5e8cb
harshitadatra <harshitkiran22@gmail.com>
2025-02-16 12:08:23 +0530

    feat: index file added

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const pollRoutes = require("./routes/pollRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/polls", pollRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
